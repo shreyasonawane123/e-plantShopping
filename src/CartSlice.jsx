7f06a57453441d3da0eb588c0db04d2e5791be86
Shreya Sonawnae <sonawaneshreya36@gmail.com>
2026-04-24 13:02:37 +0530

    Refactor CartSlice to use initialState variable

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,23 +1,55 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const CartSlice = createSlice({
+const initialState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
   name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
+  initialState,
+
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const item = action.payload;
 
-    
+      const existingItem = state.items.find(
+        (plant) => plant.name === item.name
+      );
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          ...item,
+          quantity: 1,
+        });
+      }
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload
+      );
+    },
+
+    updateQuantity: (state, action) => {
+      const { name, amount } = action.payload;
+
+      const item = state.items.find(
+        (item) => item.name === name
+      );
+
+      if (item) {
+        item.quantity = amount;
+      }
     },
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export const {
+  addItem,
+  removeItem,
+  updateQuantity,
+} = cartSlice.actions;
 
-export default CartSlice.reducer;
+export default cartSlice.reducer;
